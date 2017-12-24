ff24f941cda6fa76cae3a59bd176bbb8e6a29667
Andrea Trogolo <trogolo.andrea@seat.it>
2017-12-24 22:04:34 +0100

    Removed contacts page

--- js/init.js
+++ js/init.js
@@ -4,5 +4,9 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    $($('#nav-mobile a')[0]).on('click', function(){
+      $('.button-collapse').sideNav('hide');
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
